a80c068808e09e55bf12f08f6e60a646be213bb6
spike1207 <pjefferyorme@gmail.com>
2019-10-21 20:06:19 -0500

    adds

---
+++ manymethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace manymethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
