a83732498750779b28f67189271e1659f82ed897
spike1207 <pjefferyorme@gmail.com>
2019-10-28 20:31:07 -0500

    new file

---
+++ c#-projects/mastermind/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
