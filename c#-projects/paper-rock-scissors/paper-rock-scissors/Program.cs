dc2e79c63eace6100100913786dcdc6b8fc3c6cb
spike1207 <pjefferyorme@gmail.com>
2020-01-21 18:37:28 -0600

    added titactoe

--- c#-projects/paper-rock-scissors/paper-rock-scissors/Program.cs
+++ c#-projects/paper-rock-scissors/paper-rock-scissors/Program.cs
@@ -6,7 +6,121 @@ namespace paper_rock_scissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            string inputplayer, inputCPU;
+            int randomInt;
+            
+            bool playAgian = true;
+            
+            while (playAgian)
+            {
+                int scoreplayer = 0;
+                int scoreCPU = 0;
+
+                while (scoreplayer < 3 && scoreCPU < 3)
+                {
+
+                    Console.Write("choose between rock, paper and scissors:   ");
+                    inputplayer =
+                        Console.ReadLine();
+                    inputplayer = inputplayer.ToUpper();
+
+                    Random rnd = new Random();
+                    
+                    randomeInt = rnd.Next(1, 4);
+
+                    switch (randomInt)
+                    {
+                        case 1:
+                            inputCPU = "rock";
+                    Console.WriteLine("computer chose rock");
+                            if (inputplayer == "rock")
+                            {
+                                Console.WriteLine("draw!!\n\n");
+                            }
+                            else if (inputplayer == "paper")
+                            {
+                                Console.WriteLine("playerwins!!\n\n");
+                                scoreplayer++;
+                            }
+                            else if (inputplayer == "scissors")
+                            {
+                                Console.WriteLine("CPU wins!!\n\n");
+                                scoreCPU++;
+                            }
+                            break;
+                        case 2:
+                            inputCPU = "paper";
+                            Console.WriteLine("computer chose paper");
+                            if (inputplayer == "rock")
+                            {
+                                Console.WriteLine("CPU wins\n\n");
+                                scoreCPU++;
+                            }
+                            else if (inputplayer == "scissore") ;
+                            {
+                                Console.WriteLine("player wins\n\n")
+                                scoreplayer++;
+                            }
+                            else if (inputplayer == "paper")
+                                (
+
+                                 Console.WriteLine("draw!!\n\n");
+                                 )
+                            break;
+                        case 3:
+                            inputCPU = "scissors";
+                            Console.writeline("computer chose scissors");
+                            if (inputplayer == "scissors")
+                            {
+                                Console.WriteLine("draw!!\n\n");
+                            }
+                            else if(inputplayer == "paper")
+                            {
+                                Console.WriteLine("cpu wins!!\n\n");
+                                scoreCPU++;
+                            }
+                            else if (inputplayer == "rock")
+                            {
+                                Console.WriteLine("player wins!!\n\n");
+                                scoreplayer++;
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("invalid entry");
+                            break;
+                            
+                    }
+                    Console.WriteLine("/n/nscores:\tplayer:\t{0}\tcpu:\t{1}", scoreplayer, scoreCPU);
+                }
+                if (scoreplayer == 3)
+                {
+                    Console.WriteLine("player won");
+                }
+                else if (scoreCPU == 3)
+                {
+                    Console.WriteLine("cpu won");
+                }
+                else
+                {
+
+                }
+                Console.WriteLine("Do you want to play agian? (y/n)");
+                string loop = Console.ReadLine();
+                if (loop == "y") ;
+                {
+                    playAgian = true;
+                    Console.Clear();
+                }
+                else if (loop == "n")
+                {
+                    playAgian = false;
+                }
+                else
+                {
+
+                }
+            }
+
+            
     }
 }
--- c#-projects/tic-tac-toe/Program.cs
+++ c#-projects/tic-tac-toe/Program.cs
@@ -4,9 +4,63 @@ namespace tic_tac_toe
 {
     class Program
     {
+        static char[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        static int player = 1;
+        static int choice;
+
+        static FlagsAttribute = 0;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+                (
+                Console.clean();
+            Console.WriteLine("player1:X and and player2:O");
+            Console.WriteLine("\n");
+            if (player % 2 == 0)
+            {
+                Console.WriteLine("player 2 chance");
+            }
+            else
+            {
+                Console.WriteLine('Player 1 chance');
+            }
+            Console.WriteLine("\n");
+            boar();
+            choice = int.Parse(Console.ReadLine());
+
+            if (arr[choice] != 'X' && arr[choice] != 'O')
+            {
+                if (player % 2 == 0)
+                {
+                    arr[choice] = 'O';
+                    player++;
+                }
+                else
+                {
+                    arr[choice] = 'X';
+                    player++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("sorry the row (0) is already marked with (1)", choice, arr[choice]);
+                Console.WriteLine("\n");
+                Console.WriteLine("please wait 2 seconds, board is loading again...");
+                ThreadStaticAttribute.sleep(2000);
+            }
+            FlagsAttribute = checkwin();
+        }while (FlagsAttribute!= 1 && flag !=);
+
+          console.clean();
+            board();
+
+        if (flag ==1)
+            {
+ Console.WriteLine("player {0} has won");
+            }
+           else
+             Console.WriteLine("draw");
         }
-    }
+        
 }
