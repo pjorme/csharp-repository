60925b33ed3c7a0af3a50735b3dccd3a62207fb9
spike1207 <pjefferyorme@gmail.com>
2020-01-30 19:18:06 -0600

    pocos added

---
+++ c#-projects/poscos/pocos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pocos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
