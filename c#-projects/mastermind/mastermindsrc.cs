282d59a5ae181cd231a1144f7bc39a679a31012d
spike1207 <pjefferyorme@gmail.com>
2020-02-11 20:07:19 -0600

    tictactoe done

--- c#-projects/mastermind/mastermindsrc.cs
+++ c#-projects/mastermind/mastermindsrc.cs
@@ -4,9 +4,6 @@ namespace mastermind
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      
     }
 }
--- c#-projects/tic-tac-toe/Program.cs
+++ c#-projects/tic-tac-toe/Program.cs
@@ -26,7 +26,7 @@ namespace tic_tac_toe
                     Console.WriteLine("Player 1 chance");
                 }
                 Console.WriteLine("\n");
-                Board();
+                buildBoard();
                 choice = int.Parse(Console.ReadLine());
 
                 if (arr[choice] != 'X' && arr[choice] != 'O')
@@ -47,14 +47,14 @@ namespace tic_tac_toe
                     Console.WriteLine("sorry the row (0) is already marked with (1)", choice, arr[choice]);
                     Console.WriteLine("\n");
                     Console.WriteLine("please wait 2 seconds, board is loading again...");
-                    ThreadStaticAttribute.Sleep(2000);
+                   
                 }
-                Flag = checkwin();
-            } while (Flag != 1 && flag !=);
+                Flag = checkWin();
+            } while (Flag != 1 && Flag != -1);
 
             Console.Clear();
 
-            if (flag == 1)
+            if (Flag == 1)
             {
                 Console.WriteLine("player {0} has won");
             }
@@ -62,117 +62,88 @@ namespace tic_tac_toe
                 Console.WriteLine("draw");
         }
 
-        public static int buildBoard()
+        public static void buildBoard()
+        {
+            Console.WriteLine("  |   |   ");
+            Console.WriteLine(" {0} | {1} | {2}", arr[1], arr[2], arr[3]);
+
+            Console.WriteLine("____|____|____");
+            Console.WriteLine("  |   |   ");
+            Console.WriteLine(" {0} | {1} | {2}", arr[4], arr[5], arr[6]);
+
+            Console.WriteLine("____|____|____");
+            Console.WriteLine("  |   |   ");
+            Console.WriteLine(" {0} | {1} | {2}", arr[7], arr[8], arr[9]);
+
+        }
+
+        public static int checkWin()
         {
             #region Horzontal Winning Condtion
 
-            
             if (arr[1] == arr[2] && arr[2] == arr[3])
 
             {
-
                 return 1;
-
             }
-
-            
-
             else if (arr[4] == arr[5] && arr[5] == arr[6])
 
             {
-
                 return 1;
-
             }
-
-            
-
             else if (arr[6] == arr[7] && arr[7] == arr[8])
 
             {
-
                 return 1;
-
             }
 
-            #endregion
-
-
+            #endregion Horzontal Winning Condtion
 
             #region vertical Winning Condtion
-
-                 
 
             else if (arr[1] == arr[4] && arr[4] == arr[7])
 
             {
-
                 return 1;
-
             }
-
-            //Winning Condition For Second Column  
-
             else if (arr[2] == arr[5] && arr[5] == arr[8])
 
             {
-
                 return 1;
-
             }
-
-            //Winning Condition For Third Column  
-
             else if (arr[3] == arr[6] && arr[6] == arr[9])
 
             {
-
                 return 1;
-
             }
 
-            #endregion
-
-
+            #endregion vertical Winning Condtion
 
             #region Diagonal Winning Condition
 
             else if (arr[1] == arr[5] && arr[5] == arr[9])
 
             {
-
                 return 1;
-
             }
-
             else if (arr[3] == arr[5] && arr[5] == arr[7])
 
             {
-
                 return 1;
-
             }
 
-            #endregion
-            #region Checking For Draw
+            #endregion Diagonal Winning Condition
 
+            #region Checking For Draw
             else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
 
             {
-
                 return -1;
-
             }
-
             #endregion
 
+            return 0;
 
-
-            else
-
-            {
-
-                return 0;
-            }
+        }
     }
 }
